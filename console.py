18c8ccdbf73d07bf46320c7d4316b59be9961fa9
OrionDooms <Orion45.dd@gmail.com>
2024-03-19 05:52:15 -0700

    update console

--- console.py
+++ console.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Console Module """
+"""Console Module"""
 import cmd
 import sys
+import re
+import os
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
